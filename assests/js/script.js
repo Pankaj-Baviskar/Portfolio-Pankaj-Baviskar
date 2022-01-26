56e1f3a0c1a8eb2d7585b44cc2c25529a9df9912
Pankaj Baviskar <59195232+baviskarpankaj143@users.noreply.github.com>
2022-01-26 19:56:54 +0530

    Update script.js

--- assests/js/script.js
+++ assests/js/script.js
@@ -98,15 +98,17 @@ document.onkeydown = function(e) {
 }
 
 // Start of Tawk.to Live Chat
+<script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-  (function(){
-  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-  s1.async=true;
-  s1.src='https://embed.tawk.to/60f70460649e0a0a5ccd22a7/1fb2ei71o';
-  s1.charset='UTF-8';
-  s1.setAttribute('crossorigin','*');
-  s0.parentNode.insertBefore(s1,s0);
-  })();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/61f158a6b9e4e21181bc0336/1fqbc4k33';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
 // End of Tawk.to Live Chat
 
 
